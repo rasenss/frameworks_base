6598f9281ea9bc23b7dac83455f6d43fbe64dff6
703joko <703joko@gmail.com>
2022-03-20 15:37:58 -0400

    SystemUI: add option force media player expanded [1/2]
    Signed-off-by: 703joko <703joko@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6466,6 +6466,12 @@ public final class Settings {
         public static final String PULSE_LIGHT_LAYOUT_STYLE = "pulse_light_layout_style";
 
         /**
+         * force expand qs media player
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_FORCE_EXPAND = "artwork_media_force_expand";
+
+         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
